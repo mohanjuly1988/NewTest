77a59d9abddf021f7e3fda756c4b136bf01a5c85
mohanjuly1988 <mohanjuly1988@gmail.com>
2025-05-05 11:18:44 +0530

    Manager commit

---
+++ src/testing/Launch_Test.java
@@ -0,0 +1,28 @@
+package testing;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch_Test 
+{
+
+	public static void main(String[] args) 
+	{
+
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Selenium Java\\Testing\\Test\\chromedriver-win32\\chromedriver.exe");
+		
+		WebDriver WebObj = new ChromeDriver();
+		//WebObj.get("https://www.spicejet.com/");
+		
+		//System.out.println(WebObj.getWindowHandles().size());
+		//WebObj.switchTo().newWindow(WindowType.TAB);
+		WebObj.get("https://rahulshettyacademy.com/locatorspractice/");
+		WebObj.findElement(By.xpath("//div[contains(@class,'container')][2]/form/input[1]")).sendKeys("Hello");
+		//System.out.println(WebObj.getWindowHandles().size());
+		//System.out.println(WebObj.getCurrentUrl());
+		//WebObj.close();
+		//WebObj.quit();
+		
+	}
+
+}
